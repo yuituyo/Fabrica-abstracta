62fe5674b30f058d3ba98e3fe3238ca97cfb1962
IVAN SIQUEIROS <yuituyo@hotmail.com>
2026-03-11 22:35:21 -0700

    quitar sin referencias elegir

--- Fabrica abstracta/Elegir.cs
+++ Fabrica abstracta/Elegir.cs
@@ -20,58 +20,9 @@ namespace Fabrica_abstracta
             _anterior = anterior;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-            Mexicano mex1 = new Mexicano(this);
-            mex1.Show();
-            this.Hide();
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-            Chino chino1 = new Chino(this);
-            chino1.Show();
-            this.Hide();
-        }
-
         private void Elegir_FormClosed(object sender, FormClosedEventArgs e)
         {
             _anterior.Show();
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-            Japones japo1 = new Japones(this);
-            japo1.Show();
-            this.Hide();
-        }
-
-        private void pictureBox4_Click(object sender, EventArgs e)
-        {
-            Italiano ita = new Italiano(this);
-            ita.Show();
-            this.Hide();
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-            Italiano ita = new Italiano(this);
-            ita.Show();
-            this.Hide();
-        }
-
-        private void pictureBox5_Click(object sender, EventArgs e)
-        {
-            Americanocs americanos1 = new Americanocs(this);
-            americanos1.Show();
-            this.Hide();
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-            Americanocs americanos1 = new Americanocs(this);
-            americanos1.Show();
-            this.Hide();
         }
 
         private void label6_Click(object sender, EventArgs e)
