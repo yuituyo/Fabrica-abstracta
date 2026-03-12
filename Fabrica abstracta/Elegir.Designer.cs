45772b148207b23b0bcc6c8de2b0cb07fb203975
Esmeghdez <l21210374@tijuana.tecnm.mx>
2026-03-11 22:40:56 -0700

    menu

--- Fabrica abstracta/Elegir.Designer.cs
+++ Fabrica abstracta/Elegir.Designer.cs
@@ -57,10 +57,11 @@
             // 
             this.label6.BackColor = System.Drawing.SystemColors.Window;
             this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(447, 132);
+            this.label6.Font = new System.Drawing.Font("Baskerville Old Face", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(656, 203);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(139, 43);
+            this.label6.Size = new System.Drawing.Size(208, 66);
             this.label6.TabIndex = 5;
             this.label6.Text = "\"Menu\"";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -68,10 +69,9 @@
             // pictureBox7
             // 
             this.pictureBox7.Image = global::Fabrica_abstracta.Properties.Resources.chinaa;
-            this.pictureBox7.Location = new System.Drawing.Point(458, 348);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox7.Location = new System.Drawing.Point(687, 535);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(111, 32);
+            this.pictureBox7.Size = new System.Drawing.Size(166, 49);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 15;
             this.pictureBox7.TabStop = false;
@@ -80,10 +80,9 @@
             // pictureBox8
             // 
             this.pictureBox8.Image = global::Fabrica_abstracta.Properties.Resources.meexxx;
-            this.pictureBox8.Location = new System.Drawing.Point(106, 348);
-            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox8.Location = new System.Drawing.Point(159, 535);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(147, 32);
+            this.pictureBox8.Size = new System.Drawing.Size(220, 49);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 14;
             this.pictureBox8.TabStop = false;
@@ -92,10 +91,9 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::Fabrica_abstracta.Properties.Resources.usa_removebg_preview;
-            this.pictureBox5.Location = new System.Drawing.Point(595, 428);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox5.Location = new System.Drawing.Point(892, 658);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(171, 127);
+            this.pictureBox5.Size = new System.Drawing.Size(256, 195);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 12;
             this.pictureBox5.TabStop = false;
@@ -103,10 +101,9 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::Fabrica_abstracta.Properties.Resources.italia;
-            this.pictureBox4.Location = new System.Drawing.Point(261, 410);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox4.Location = new System.Drawing.Point(392, 631);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(169, 157);
+            this.pictureBox4.Size = new System.Drawing.Size(254, 242);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 11;
             this.pictureBox4.TabStop = false;
@@ -114,10 +111,9 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Fabrica_abstracta.Properties.Resources.japones;
-            this.pictureBox3.Location = new System.Drawing.Point(737, 187);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(1106, 288);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(169, 157);
+            this.pictureBox3.Size = new System.Drawing.Size(254, 242);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
@@ -125,10 +121,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Fabrica_abstracta.Properties.Resources.china;
-            this.pictureBox2.Location = new System.Drawing.Point(436, 187);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(654, 288);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(150, 148);
+            this.pictureBox2.Size = new System.Drawing.Size(225, 228);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
@@ -136,10 +131,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Fabrica_abstracta.Properties.Resources.mexico;
-            this.pictureBox1.Location = new System.Drawing.Point(97, 187);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(146, 288);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(169, 157);
+            this.pictureBox1.Size = new System.Drawing.Size(254, 242);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -147,10 +141,9 @@
             // pictureBox6
             // 
             this.pictureBox6.Image = global::Fabrica_abstracta.Properties.Resources.image;
-            this.pictureBox6.Location = new System.Drawing.Point(458, 14);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox6.Location = new System.Drawing.Point(678, 12);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(111, 116);
+            this.pictureBox6.Size = new System.Drawing.Size(166, 178);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 7;
             this.pictureBox6.TabStop = false;
@@ -158,10 +151,9 @@
             // pictureBox9
             // 
             this.pictureBox9.Image = global::Fabrica_abstracta.Properties.Resources.japoonn;
-            this.pictureBox9.Location = new System.Drawing.Point(744, 348);
-            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox9.Location = new System.Drawing.Point(1116, 535);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(157, 32);
+            this.pictureBox9.Size = new System.Drawing.Size(236, 49);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox9.TabIndex = 16;
             this.pictureBox9.TabStop = false;
@@ -170,10 +162,9 @@
             // pictureBox10
             // 
             this.pictureBox10.Image = global::Fabrica_abstracta.Properties.Resources.italiaaa;
-            this.pictureBox10.Location = new System.Drawing.Point(273, 571);
-            this.pictureBox10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox10.Location = new System.Drawing.Point(410, 878);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(147, 32);
+            this.pictureBox10.Size = new System.Drawing.Size(220, 49);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox10.TabIndex = 17;
             this.pictureBox10.TabStop = false;
@@ -182,10 +173,9 @@
             // pictureBox11
             // 
             this.pictureBox11.Image = global::Fabrica_abstracta.Properties.Resources.americaaa;
-            this.pictureBox11.Location = new System.Drawing.Point(611, 571);
-            this.pictureBox11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox11.Location = new System.Drawing.Point(916, 878);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(147, 32);
+            this.pictureBox11.Size = new System.Drawing.Size(220, 49);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox11.TabIndex = 18;
             this.pictureBox11.TabStop = false;
@@ -193,10 +183,10 @@
             // 
             // Elegir
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(977, 632);
+            this.ClientSize = new System.Drawing.Size(1466, 972);
             this.Controls.Add(this.pictureBox11);
             this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox9);
@@ -209,6 +199,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.label6);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Elegir";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Elegir";
